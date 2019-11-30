fa786f8d6d37fd7629be217d1107b6aa9d9a8def
mode-x <abiaemma@gmail.com>
2019-11-30 09:18:17 +0100

    Adding interactivity to square buttons

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,6 +2,15 @@ import React from "react"
 
 export default class Square extends React.Component {
   render() {
-    return <button className="square">{this.props.value}</button>
+    return (
+      <button
+        className="square"
+        onClick={function() {
+          alert("click")
+        }}
+      >
+        {this.props.value}
+      </button>
+    )
   }
 }
